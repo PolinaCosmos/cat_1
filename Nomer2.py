c411067caf6a1a21216792acad82f82a3a6217fb
Polina Vasileva <119327761+PolinaCosmos@users.noreply.github.com>
2023-02-13 23:40:06 +0300

    Add files via upload
    В отдельный файл импортируйте и создайте объект Cat, который выводит имеющихся на сайте котов, с одинаковыми параметрами, но с разными значениями.

---
+++ Nomer2.py
@@ -0,0 +1,11 @@
+from Nomer1 import Cat
+
+cat1 = Cat(name='Барон', sex='мальчик', age=2)
+cat2 = Cat(name='Сэм', sex='мальчик', age=2)
+
+print(cat1.getName(),
+      cat1.getSex(),
+      cat1.getAge())
+print(cat2.getName(),
+      cat2.getSex(),
+      cat2.getAge())
